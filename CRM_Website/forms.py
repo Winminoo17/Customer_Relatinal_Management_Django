a4d82bbf24bdadaa3b4d1786a8d52832ee03be31
Winminoo17 <110585405+Winminoo17@users.noreply.github.com>
2026-07-21 15:56:49 +0700

    Correct the spelling SingUpForm to SignUpForm

--- CRM_Website/forms.py
+++ CRM_Website/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class SingUpForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -12,7 +12,7 @@ class SingUpForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
         
     def __init__(self, *args, **kwargs):
-        super(SingUpForm, self).__init__(*args, **kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].lablel =''
