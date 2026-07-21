d4be389465c2ae88a499c3318413c8b547fb8cc1
Win <winminoothet@gmail.com>
2026-07-22 01:42:10 +0700

    'Final_Edition'

--- CRM_Website/forms.py
+++ CRM_Website/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
         model = User
@@ -27,3 +28,23 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].help_text = '<span class="form-text text-muted">Your Password Must match</span>'
 
 
+class AddRecordForm(forms.ModelForm):
+    # Changed TextInput to CharField
+    first_name = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    
+    # Changed EmailInput to EmailField
+    email = forms.EmailField(label="", required=True, max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+    
+    phone = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}))
+    address = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Street Address'}))
+    city = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}))
+    state = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}))
+    zipcode = forms.CharField(label="", required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zipcode'}))
+    
+    # Changed Textarea to CharField + Textarea widget
+    description = forms.CharField(label="", required=True, max_length=500, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add a description...', 'rows': 4}))
+    
+    class Meta:
+        model = Record
+        exclude = ('user',)
