3c524461c29f9387dc6ccd9a96a610caf40947ab
Win <winminoothet@gmail.com>
2026-07-21 15:58:37 +0700

    'correct_spelling'

--- CRM_Website/forms.py
+++ CRM_Website/forms.py
@@ -8,20 +8,25 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
-        mdoel = User
+        model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
         
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].lablel =''
+        self.fields['username'].label =''
         self.fields['username'].help_text = '<span class="form-text text-muted">Your Name</span>'
         
         self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].lablel = ''
+        self.fields['password1'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted">Your Password</span>'
         
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+<<<<<<< HEAD
         self.fields['password2'].lablel = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted">Your Password Must match</span>'
+=======
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted">Your Password Must match</span>'
+>>>>>>> 871174d ('correct_spellin')
